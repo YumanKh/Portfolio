a3230c1289363bf190114e935074e604bb226527
YumanKh <yumankhoufache607@gmail.com>
2026-09-03 19:39:47 -0500

    feat: finalize Contact Me page design

--- src/ContactMe.tsx
+++ src/ContactMe.tsx
@@ -2,9 +2,30 @@ const ContactMe = () => {
 
     return(
         <>
-            <h2>Test: Contact Me</h2>
+            <main className="contact-me-main">
+                <div className="contact-me-card">
+                    <h3>Contact Me</h3>
+                    <h2>Name</h2>
+                    <input
+                        type="text"
+                        placeholder="Enter your name..."
+                    />
+                    <h2>Email</h2>
+                    <input
+                        type="email"
+                        placeholder="Enter your email..."
+                    />
+                    <h2>Message</h2>
+                    <input 
+                        className="message-input"
+                        type="text"
+                        placeholder="Enter your message..."
+                    />
+                    
+                </div>
+            </main>
         </>
     )
 }
 
-export default ContactMe;
+export default ContactMe
